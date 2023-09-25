c7665d8fe998774021e685f12a04de9051094ebc
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2023-09-25 22:41:08 +0500

    Updating file

--- Class 5/index.ts
+++ Class 5/index.ts
@@ -18,41 +18,35 @@
 //     }
 // }])
 // console.log("your name is : ", answer.userName);
-
 // import sumOfTwo from "./sum.js";
 // import productOfTwo from "./multiply.js";
 // import differenceOfTwo from "./subtract.js";
-
-
 // console.log(sumOfTwo(15,20));
-
 // console.log(productOfTwo(5, 10))
-
 // console.log(differenceOfTwo(20, 10))
 
 
 import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+{
+    name: "userName",
+    type: "string",
+    message: "Enter your name: "
+},
+{
+    name: "email",
+    type: "string",
+    message: "Enter your email"
+},
+{
+    name:"contact",
+    type:"number",
+    message: "Enter your contact"
+}
+])
 
-// let answers = await inquirer.prompt([
-// {
-//     name: "userName",
-//     type: "string",
-//     message: "Enter your name: "
-// },
-// {
-//     name: "email",
-//     type: "string",
-//     message: "Enter your email"
-// },
-// {
-//     name:"contact",
-//     type:"number",
-//     message: "Enter your contact"
-// }
-// ])
-
-// console.log(chalk.bgBlue(answers.userName))
-// console.log(answers.contact)
+console.log(chalk.bgBlue(answers.userName))
+console.log(answers.contact)
 
 import chalk from "chalk";
 
@@ -60,3 +54,9 @@ console.log(chalk.red("Hello world"))
 console.log("Hello world")
 console.log(chalk.bgBlueBright("Hello world"))
 console.log('Hello', chalk.underline.bgBlue('world') + '!')
+
+// import showBanner from 'node-banner';
+ 
+// (async () => {
+//     await showBanner('The Title', 'This is a suitable tagline');
+// })();
--- Class 5/sum.ts
+++ Class 5/sum.ts
@@ -1,8 +1,6 @@
 export default function sumOfTwo(a: number, b: number){
-
     return a+b;
     //
     // export default
     //
-
 }
