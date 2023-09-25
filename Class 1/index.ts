72c1dbd5c0ba9e6612870e7d163a48e401865754
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2023-09-25 10:00:52 +0500

    Initial repository

---
+++ Class 1/index.ts
@@ -0,0 +1,44 @@
+let myName = 'Usman'  // string dataType
+
+//let, const, var  _
+
+const age = 25;   // number 
+
+var address ="ISB";
+
+console.log("My name is ", myName);
+console.log("age is ", age);
+
+// //strongly typed syntax
+// let a : string = "Pakistan";
+// a = "USA";
+// let b : number = 9;
+// let c : boolean = true;
+
+// //type inference
+// let e = "USA";
+// let f = 10.9;
+// f = 22;
+// let g = false;
+// g = true;
+
+// //use const where variable values do not change
+// const a = 5;
+// const b : number = 33;
+// const c ="best";
+
+// //I suggest use let instead of var everywhere, 
+// //becuase let has blocked scope
+// if (true) {
+// 	let z = 4;
+// 	//use z
+// }
+// else {
+// 	let z = "string";
+// 	//use z
+// }
+// console.log("let: " + z);// Error: z is not defined in this scope
+
+
+
+
