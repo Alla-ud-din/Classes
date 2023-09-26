64639ef7329f3b7673ad681e6096501c7c3937b8
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2023-09-26 06:24:58 +0500

    Updating file

--- Class 5/index.js
+++ Class 5/index.js
@@ -24,31 +24,32 @@
 // console.log(sumOfTwo(15,20));
 // console.log(productOfTwo(5, 10))
 // console.log(differenceOfTwo(20, 10))
-import inquirer from "inquirer";
-let answers = await inquirer.prompt([
-    {
-        name: "userName",
-        type: "string",
-        message: "Enter your name: "
-    },
-    {
-        name: "email",
-        type: "string",
-        message: "Enter your email"
-    },
-    {
-        name: "contact",
-        type: "number",
-        message: "Enter your contact"
-    }
-]);
-console.log(chalk.bgBlue(answers.userName));
-console.log(answers.contact);
-import chalk from "chalk";
-console.log(chalk.red("Hello world"));
-console.log("Hello world");
-console.log(chalk.bgBlueBright("Hello world"));
-console.log('Hello', chalk.underline.bgBlue('world') + '!');
+export {};
+// import inquirer from "inquirer";
+// let answers = await inquirer.prompt([
+// {
+//     name: "userName",
+//     type: "string",
+//     message: "Enter your name: "
+// },
+// {
+//     name: "email",
+//     type: "string",
+//     message: "Enter your email"
+// },
+// {
+//     name:"contact",
+//     type:"number",
+//     message: "Enter your contact"
+// }
+// ])
+// console.log(chalk.bgBlue(answers.userName))
+// console.log(answers.contact)
+// import chalk from "chalk";
+// console.log(chalk.red("Hello world"))
+// console.log("Hello world")
+// console.log(chalk.bgBlueBright("Hello world"))
+// console.log('Hello', chalk.underline.bgBlue('world') + '!')
 // import showBanner from 'node-banner';
 // (async () => {
 //     await showBanner('The Title', 'This is a suitable tagline');
--- Class 5/index.ts
+++ Class 5/index.ts
@@ -26,34 +26,34 @@
 // console.log(differenceOfTwo(20, 10))
 
 
-import inquirer from "inquirer";
-let answers = await inquirer.prompt([
-{
-    name: "userName",
-    type: "string",
-    message: "Enter your name: "
-},
-{
-    name: "email",
-    type: "string",
-    message: "Enter your email"
-},
-{
-    name:"contact",
-    type:"number",
-    message: "Enter your contact"
-}
-])
+// import inquirer from "inquirer";
+// let answers = await inquirer.prompt([
+// {
+//     name: "userName",
+//     type: "string",
+//     message: "Enter your name: "
+// },
+// {
+//     name: "email",
+//     type: "string",
+//     message: "Enter your email"
+// },
+// {
+//     name:"contact",
+//     type:"number",
+//     message: "Enter your contact"
+// }
+// ])
 
-console.log(chalk.bgBlue(answers.userName))
-console.log(answers.contact)
+// console.log(chalk.bgBlue(answers.userName))
+// console.log(answers.contact)
 
-import chalk from "chalk";
+// import chalk from "chalk";
 
-console.log(chalk.red("Hello world"))
-console.log("Hello world")
-console.log(chalk.bgBlueBright("Hello world"))
-console.log('Hello', chalk.underline.bgBlue('world') + '!')
+// console.log(chalk.red("Hello world"))
+// console.log("Hello world")
+// console.log(chalk.bgBlueBright("Hello world"))
+// console.log('Hello', chalk.underline.bgBlue('world') + '!')
 
 // import showBanner from 'node-banner';
  
