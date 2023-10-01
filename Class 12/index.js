40b8b0b2817fc4bdd1faea12284de4f4c163c472
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2023-10-01 11:58:06 +0500

    uploading file

--- Class 12/index.js
+++ Class 12/index.js
@@ -1,19 +1,14 @@
 // // Abstration 
-
 // // Things we can't define at that time
-
 // // abstract class Base {
 // //     foo(): number { return this.bar(); }
 // //     abstract bar() : number;
 // // }
-
 // // class E extends Base { // okay -- implements abstract method
 // //     bar() { return 1; }
 // // }
-
 // // // var obj1 = new Base();//Error, cannnot create a instance of a Abstract class
 // // // console.log(obj1.foo());
-
 // abstract class Animal {
 //     abstract sound(): void;    // abstract class ka object/instance create nai hota
 //     abstract move(): void;     // Thus we have to add  "abstract" with class too
@@ -33,30 +28,21 @@
 //         console.log("Dog is Libra")
 //     }
 // }           // ?
-
-
 // let dg = new libra();
-
 // dg.move();
 // dg.sound();
-
-
 // // <========================= interface ================>
-
 // interface Shape {
 //     color: string;
 //     size: number;
 // }
-
 // interface Square extends Shape {
 //     sideLength: number;
 // }
-
 // // var square = <Square>{};
 // // square.color = "blue";
 // // square.sideLength = 10;
 // // square.a = 5;//Error
-
 // class circle implements Square {
 //     color: string = "red";
 //     size: number = 10;
@@ -66,6 +52,3 @@
 // console.log(circleObj.sideLength)
 // console.log(circleObj.size)
 // console.log(circleObj.color)
-
-
-
